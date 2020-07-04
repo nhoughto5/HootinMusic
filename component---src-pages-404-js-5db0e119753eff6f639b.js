7a7bbeaf38b8cc0c2abbf5c402a957270ebe165d
Nick Houghton <nhoughto5@gmail.com>
2020-07-04 09:56:50 -0700

    Updates

---
+++ component---src-pages-404-js-5db0e119753eff6f639b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Bl7J"),o=n("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("div",{className:"notFound"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5db0e119753eff6f639b.js.map
