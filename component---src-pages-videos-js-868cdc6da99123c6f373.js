bed6fe740ff9b8ab669451b975acefd59cf327f2
Nick Houghton <nhoughto5@gmail.com>
2021-02-18 09:17:33 -0800

    Updates

--- component---src-pages-videos-js-868cdc6da99123c6f373.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"5AIy":function(e,t,a){"use strict";a.r(t);var n=a("Wbzz"),o=a("q1tI"),l=a.n(o),c=a("Bl7J"),i=a("vrFN");t.default=function(){return l.a.createElement(c.a,{pageInfo:{pageName:"videos"}},l.a.createElement(i.a,{title:"Videos"}),l.a.createElement("h1",null,"Hi from the video page"),l.a.createElement("p",null,"Welcome to the video page"),l.a.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-videos-js-868cdc6da99123c6f373.js.map
